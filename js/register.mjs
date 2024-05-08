db85f26dee5ffae3c4962e1b06f7f270e79ec7de
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-05-08 16:53:35 +0200

    Adds js to register page

---
+++ js/register.mjs
@@ -0,0 +1,69 @@
+import { API_BASE_URL } from "./utils/api.mjs";
+
+//Dropdown menu
+const menuBtn = document.getElementById("menu-btn");
+const dropdownMenu = document.getElementById("dropdown");
+
+dropdownMenu.classList.remove("visible");
+
+function toggleDropdown() {
+  dropdownMenu.classList.toggle("visible");
+}
+
+menuBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
+  toggleDropdown();
+});
+
+document.documentElement.addEventListener("click", (e) => {
+  if (dropdownMenu.classList.contains("visible")) {
+    toggleDropdown();
+  }
+});
+
+//Register form
+const registerForm = {
+  username: document.getElementById("username-input"),
+  avatar: document.getElementById("avatar-input"),
+  email: document.getElementById("email-input"),
+  password: document.getElementById("password-input"),
+  submit: document.getElementById("register-btn"),
+};
+
+const registerUrl = `${API_BASE_URL}auth/register`;
+
+registerForm.submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  registerUser(registerUrl);
+});
+
+async function registerUser(url) {
+  try {
+    const data = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: registerForm.username.value,
+        avatar: {
+          url: registerForm.avatar.value,
+        },
+        email: registerForm.email.value,
+        password: registerForm.password.value,
+      }),
+    };
+    const res = await fetch(url, data);
+    console.log(res);
+    const json = await res.json();
+    console.log(json);
+    if (json.errors) {
+      const obj = json.errors;
+      for (let i = 0; i < obj.length; i++) alert(obj[i].message);
+    } else {
+      alert("Thank you for registering!");
+    }
+  } catch (error) {
+    alert("Error registering", error);
+  }
+}
