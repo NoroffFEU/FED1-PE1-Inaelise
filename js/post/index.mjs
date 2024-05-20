4b4db4fbc9a2473ca77a232b7bb5f4f85e724809
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-05-20 13:01:24 +0200

    Adds html for post page

--- js/post/index.mjs
+++ js/post/index.mjs
@@ -20,7 +20,44 @@ document.documentElement.addEventListener("click", (e) => {
   }
 });
 
-/* function renderPostPageHtml(post) {} */
+function renderPostPageHtml(post) {
+  const article = document.querySelector(".post-container");
+  const info = document.querySelector(".article-info");
+  const content = document.querySelector(".article-content");
+
+  const image = document.createElement("img");
+  image.src = post.media.url;
+  image.alt = post.media.alt;
+  image.classList.add("post-img");
+
+  const authorImg = document.createElement("img");
+  authorImg.src = post.author.avatar.url;
+  authorImg.alt = post.author.avatar.alt;
+  authorImg.classList.add("author-img");
+
+  const contentInfo = document.createElement("div");
+  contentInfo.classList.add("content-info");
+  const author = document.createElement("p");
+  author.classList.add("author");
+  author.textContent = post.author.name;
+  const likes = document.createElement("p");
+  likes.textContent = "16 likes";
+  const comments = document.createElement("p");
+  comments.textContent = "4 comments";
+
+  const contentDiv = document.createElement("div");
+  contentDiv.classList.add("content-div");
+  const date = document.createElement("p");
+  const isoDate = new Date(post.updated);
+  const localeFormat = isoDate.toLocaleDateString("en-GB");
+  date.textContent = `Last updated: ${localeFormat}`;
+
+  contentDiv.append(date);
+  contentInfo.append(author, likes, comments);
+  content.append(contentInfo, contentDiv);
+  info.append(authorImg);
+  article.append(image, info, content);
+}
 
 const postId = new URLSearchParams(window.location.search).get("id");
 const postUrl = `${API_BASE_URL}blog/posts/OlaNordmann/${postId}`;
@@ -37,11 +74,9 @@ async function getPost() {
   throw new Error(json.message);
 }
 
-getPost();
-
-/* async function renderPostPage() {
+async function renderPostPage() {
   const post = await getPost();
   renderPostPageHtml(post);
 }
 
-renderPostPage(); */
+renderPostPage();
