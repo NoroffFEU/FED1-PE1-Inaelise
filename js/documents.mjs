99576268dcfa8de3d12eea5c9938796f2e915d31
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-05-29 15:50:25 +0200

    Adds js to pages

---
+++ js/documents.mjs
@@ -0,0 +1,9 @@
+import { greeting } from "./utils/greeting.mjs";
+import { logout } from "./utils/logout.mjs";
+import { showDropdown } from "./utils/dropdown.mjs";
+import { showCreateLink } from "./utils/owner.mjs";
+
+showCreateLink();
+showDropdown();
+greeting();
+logout();
