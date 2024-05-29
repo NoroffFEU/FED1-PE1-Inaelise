f11794d8fda7c116048562518ca482caf5906c91
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-05-29 16:07:35 +0200

    Adds another function with different path

--- js/utils/logout.mjs
+++ js/utils/logout.mjs
@@ -1,6 +1,6 @@
 import { loadStorage, removeFromStorage } from "./localStorage.mjs";
 
-export function logout() {
+export function logoutOne() {
   const user = loadStorage("user");
   const button = document.querySelector(".logout-btn");
   const container = document.querySelector(".logout");
@@ -13,7 +13,25 @@ export function logout() {
       removeFromStorage("user");
       removeFromStorage("accessToken");
       alert("You've logged out!");
-      window.location.reload();
+      window.location.href = "./index.html";
+    }
+  });
+}
+
+export function logoutTwo() {
+  const user = loadStorage("user");
+  const button = document.querySelector(".logout-btn");
+  const container = document.querySelector(".logout");
+
+  if (user) {
+    container.classList.remove("hidden-link");
+  }
+  button.addEventListener("click", () => {
+    if (window.confirm("Are you sure you want to logout?") === true) {
+      removeFromStorage("user");
+      removeFromStorage("accessToken");
+      alert("You've logged out!");
+      window.location.href = "../index.html";
     }
   });
 }
