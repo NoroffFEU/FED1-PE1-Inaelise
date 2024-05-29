343b41b780eaaeffc70768fb72ff7284a0e348d2
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-05-29 16:32:30 +0200

    Adds condition for when user is already logged in

--- js/account/login.mjs
+++ js/account/login.mjs
@@ -2,7 +2,7 @@ import { API_BASE_URL } from "../utils/api.mjs";
 import { showDropdown } from "../utils/dropdown.mjs";
 import { greeting } from "../utils/greeting.mjs";
 import * as storage from "../utils/localStorage.mjs";
-import { logoutTwo } from "../utils/logout.mjs";
+import { logoutTwo, logoutThree } from "../utils/logout.mjs";
 import { showCreateLink } from "../utils/owner.mjs";
 
 showDropdown();
@@ -22,6 +22,28 @@ loginForm.submit.addEventListener("click", (e) => {
   e.preventDefault();
   loginUser(loginUrl);
 });
+
+function getUser() {
+  const user = storage.loadStorage("user");
+  const form = document.querySelector(".login-form");
+  const section = document.querySelector(".bg-login");
+  if (user) {
+    form.classList.add("hidden-link");
+    const userExist = document.createElement("h2");
+    userExist.classList.add("logged-in");
+    userExist.textContent = `You're already logged in as ${user.name}.`;
+
+    const logout = document.createElement("div");
+    logout.classList.add("logout-div");
+    logout.innerHTML = `<p>Do you want to logout?</p><button class="logout-btn nav-link" id="page-logout" title="Click to logout">
+    logout
+  </button>`;
+    section.append(userExist, logout);
+    logoutThree();
+  }
+}
+
+getUser();
 
 async function loginUser(url) {
   try {
